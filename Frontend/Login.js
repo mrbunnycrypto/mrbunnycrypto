39bb10a5c5d0a0c343b791b2eff4495c22c4bf81
mrbunnycrypto <mybunnycrypto@gmail.com>
2025-02-19 18:17:05 +0800

    Create Login.js

---
+++ Frontend/Login.js
@@ -0,0 +1,24 @@
+/* Frontend - Login.js (React) */
+import React, { useState } from "react";
+import axios from "axios";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("/api/login", { email, password });
+    localStorage.setItem("token", response.data.token);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
